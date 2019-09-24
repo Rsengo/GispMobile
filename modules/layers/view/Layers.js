87eb5cad786991bac7918908734a9da986077215
Rsengo <rsengo42@gmail.com>
2019-09-25 00:11:06 +0700

    refactor

--- modules/layers/view/Layers.js
+++ modules/layers/view/Layers.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import { List } from 'react-native-paper';
 import Layer from './Layer';
 import { actions as rootActions } from '../../root';
